67537e911960a66621c050c69c2a792dbe9de066
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-08-10 15:10:17 -0700

    firebase json

---
+++ backend/src/main/java/com/fran/Sistema_Inventario/Firebase/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package com.fran.Sistema_Inventario.Firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp initializeFirebaseApp() throws IOException {
+        FileInputStream serviceAccount
+                = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("gs://productos-inventario-7f2d7.appspot.com") // Reemplaza con tu bucket
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}
