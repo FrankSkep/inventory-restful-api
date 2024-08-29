86a66cb2eaf6b83d47688bb459f23108d4d037d0
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-08-29 16:15:32 -0700

    credenciales cloudinary en .env

--- src/main/java/com/fran/Sistema_Inventario/Cloudinary/CloudinaryConfig.java
+++ src/main/java/com/fran/Sistema_Inventario/Cloudinary/CloudinaryConfig.java
@@ -2,18 +2,21 @@ package com.fran.Sistema_Inventario.Cloudinary;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    private final Dotenv env = Dotenv.load();
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "",
-                "api_key", "",
-                "api_secret", ""
+                "cloud_name", env.get("CLOUDINARY_CLOUD_NAME"),
+                "api_key", env.get("CLOUDINARY_API_KEY"),
+                "api_secret", env.get("CLOUDINARY_API_SECRET")
         ));
     }
 }
