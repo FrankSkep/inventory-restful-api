796c7d642446a95c6e663e205230c0c36eb3f7a0
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-09-01 10:25:57 -0700

    migrate application.properties to .yml; use Cloudinary Config class, remove dotenv

--- src/main/java/com/fran/inventory_api/system/service/Impl/CloudinaryServiceImpl.java
+++ src/main/java/com/fran/inventory_api/system/service/Impl/CloudinaryServiceImpl.java
@@ -3,7 +3,7 @@ package com.fran.inventory_api.system.service.Impl;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.fran.inventory_api.system.service.CloudinaryService;
-import io.github.cdimascio.dotenv.Dotenv;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,18 +11,10 @@ import java.io.IOException;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class CloudinaryServiceImpl implements CloudinaryService {
 
     private final Cloudinary cloudinary;
-
-    public CloudinaryServiceImpl() {
-        Dotenv env = Dotenv.load();
-        cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", env.get("CLOUDINARY_CLOUD_NAME"),
-                "api_key", env.get("CLOUDINARY_API_KEY"),
-                "api_secret", env.get("CLOUDINARY_API_SECRET")
-        ));
-    }
 
     @Override
     public Map upload(MultipartFile file) throws IOException {
